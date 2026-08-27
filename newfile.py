1d18bbba3167ebde730277a7915fa6d17582d302
Bigsam00173 <marklarine74@gmail.com>
2026-08-27 10:49:43 +0100

    Add sanitized newfile.py

---
+++ newfile.py
@@ -0,0 +1,187 @@
+#  -*-  coding:  utf-8  -*-
+print ("Hello, World!")
+a=10
+b=2
+c=a+b
+print ("c")
+
+a=5
+b=1025
+c=a-b
+print (c)
+
+d=25
+e=d*c
+print (e)
+
+f=1000254846
+g=a*f
+print (g)
+
+h=10
+i=299
+j=h**i
+print (j)
+
+k=5
+l=2
+m=2**5
+print (m)
+
+n=d*l
+print ('n')
+
+x=j/i
+print ('x')
+
+a=80
+b=8
+c=a/b
+print ('c')
+
+d_x=a*b
+print ('d_x')
+
+fem=a
+hh=a*a
+print ('hh')
+
+tj=a//b
+print (tj)
+jc=b//a
+print ('jc')
+
+c=10/2
+if c==2:
+	print ("🤦🏾")
+elif c==85:
+	print("Not yet")
+elif c==25:
+	print ("Hmmm")
+elif c==5:
+	print ("🤷🏾")
+else:
+	print ('🙍🏾')
+	
+my_Fteam= ["Haaland", "Pep", "Debruyne"]
+print ("my_Fteam[1]")
+for genius in my_Fteam:
+	print (genius)
+
+my_Fteam.append ("Rodri")
+print ("my_Fteam")
+my_Fteam.insert (0,  "Gundogan")
+
+
+my_Fteam.append ("and Kova")
+print ("my_Fteam")
+
+my_Fteam.remove ("Pep")
+print ("my_Fteam")
+
+
+def My_siblings(Peter, Tibiebi):  #defining the My_siblings function 
+	print ("Fagha, Kimi")
+My_siblings("Peter", "Tibiebi")
+
+def Tibiebi (BigSam):   #defining the Tibiebi function 
+	print (
+	"Hey wagwan broskis!!....\n"
+	"It's Big$am here,no fake love here mehn\n"
+	"Let's be real!!... that's Big$am's vibe huh\n"
+	"Now let's go buss it down buddy ❤️🤞🏾\n"
+	)
+Tibiebi ("Bigsam")
+
+def mass_print():
+	print ("mass_print")
+mass_print()
+
+def mysiblings ():
+	print ("Fagha, Kimi")
+mysiblings ()
+
+a=5
+b=2
+c=a/b
+print (c)
+
+for num in range (1,21):
+	print ("are you good?")
+
+print (Tibiebi("Bigsam"))
+
+print (my_Fteam[:])
+
+my_Fteam[2]="Pep"
+print ("my_Fteam")
+
+dic1={'hobby' :  'play',  'bby' : 5}
+print (dic1.get("play"))
+
+print(dic1.get (5))
+
+Animals={'lion' :  'cub',  'tiger' : 'tigress'}
+print(Animals.get("cub"))
+
+a="Hello"
+b="world "
+c="Hello" + "world"
+print ('c')
+
+def Helloworld ():
+	b=Helloworld*3
+print ('b')
+
+
+def football ():
+	c=football*4
+print ('c')
+
+Combination = "Combination"
+print ('Combination'[2])
+
+Combination = "Combination"
+print ('combination [2]')
+
+a = int("20" , 4)
+print (a)
+
+a = int("10111" , 2)
+print(a)
+
+import random 
+mlist = [3,2,5,7,4,90,7,6,5]
+random.shuffle (mlist)
+print ("shuffle.random", mlist)
+
+import random 
+print(random.randint(-9,5))
+
+import random 
+print(random.randint(5,9))
+
+import random 
+print(random.randint(-9,5))
+
+import random 
+print (random.randrange(2,9))
+
+import random 
+number_list =[7,90,65,3,4,92,2,0,6,15,]
+print(random.choice(number_list))
+
+import statistics           
+scores = [2,5,3,8,9,3,2,1,9]
+a = statistics.mean(scores)
+print (a)
+
+import statistics
+scores = [4,5,7,1,8,6]
+a = statistics.mean(scores)
+print (a)
+
+import random
+myagemates = [15,17,54,89,10,32]
+random.shuffle(myagemates)
+print("shuffle.random",myagemates)
